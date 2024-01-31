1c2e0e3210936de83ad50557ee3cfe25a17663a5
Vaibhav <vaibhavchaubey786@gmail.com>
2024-01-31 16:34:25 +0530

    Add controllers and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -260,6 +260,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
