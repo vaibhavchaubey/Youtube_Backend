6f3578e4f4b62fc07433b11963d79483d57e4f58
Vaibhav <vaibhavchaubey786@gmail.com>
2024-02-02 23:44:37 +0530

    Added all comment controller endpoints

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,196 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+// get all comments for a video
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
-})
+    // Fetch the video by its ID
+    const video = await Video.findById(videoId);
 
+    // Check if the video exists
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    // Perform aggregation on the Comment collection
+    const commentsAggregate = Comment.aggregate([
+        // Match comments for the specified video
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        // Perform a lookup to get owner details from the "users" collection
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+            },
+        },
+        // Perform a lookup to get likes associated with each comment from the "likes" collection
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes",
+            },
+        },
+        // Add additional fields to each comment document
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likes",
+                },
+                owner: {
+                    $first: "$owner",
+                },
+                // Check if the currently authenticated user has liked the comment
+                isLiked: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$likes.likedBy"],
+                        },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        // Project to include only the desired fields in the output
+        {
+            $project: {
+                content: 1,
+                createdt: 1,
+                likesCount: 1,
+                owner: {
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                },
+                isLiked: 1,
+            },
+        },
+    ]);
+
+    // Pagination options
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+    };
+
+    // Use aggregatePaginate to handle pagination
+    const comments = await Comment.aggregatePaginate(
+        commentsAggregate,
+        options
+    );
+
+    // Return the comments as a JSON response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comments, "Comments fetched successfully"));
+});
+
+// add a comment to a video
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+    const { videoId } = req.params;
+    const { content } = req.body;
 
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id,
+    });
+
+    if (!comment) {
+        throw new ApiError(500, "Failed to add comment please try again");
+    }
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, comment, "Comment added successfully"));
+});
+
+// update a comment
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    if (comment?.owner.toString() !== req.user?._id) {
+        throw new ApiError(400, "only comment owner can edit their comment");
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        comment?._id,
+        {
+            $set: {
+                content,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    if (!updatedComment) {
+        throw new ApiError(500, "Failed to edit comment please try again");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedComment, "Comment edited successfully")
+        );
+});
+
+// delete a comment
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+    const { commentId } = req.params;
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
     }
+
+    if (comment?.owner.toString() !== req.user?._id) {
+        throw new ApiError(400, "only comment owner can delete their comment");
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
