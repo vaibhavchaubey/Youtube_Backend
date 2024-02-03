d26f80d503d45c5cf021bf62bf85ef642308ed13
Vaibhav <vaibhavchaubey786@gmail.com>
2024-02-03 14:11:43 +0530

    Added all subscription controller endpoints

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,19 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
     getSubscribedChannels,
     getUserChannelSubscribers,
     toggleSubscription,
-} from "../controllers/subscription.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/c/:channelId")
-    .get(getSubscribedChannels)
+    .get(getUserChannelSubscribers)
     .post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
-export default router
+export default router;
