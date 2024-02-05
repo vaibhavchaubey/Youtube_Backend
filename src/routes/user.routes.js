43e5e9fd56312bb0ff7150d1765c9d31c2efc7d2
Vaibhav <vaibhavchaubey786@gmail.com>
2024-02-05 12:52:21 +0530

    Update user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -43,13 +43,13 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 router
-    .route("/avatar")
+    .route("/update-avatar")
     .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-    .route("/cover-image")
+    .route("/update-coverImg")
     .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
-router.route("/history").get(verifyJWT, getWatchHistory);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
